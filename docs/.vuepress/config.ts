5bf8bf560ee565443ea6a23503b1556f1476a707
Zhaoji Wang <hwoam@outlook.com>
2023-02-07 07:26:04 +0800

    初始化以及添加课程介绍

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,41 @@
+import { defineUserConfig, defaultTheme, HeadConfig } from 'vuepress'
+
+const head: HeadConfig[] = [['link', { rel: 'icon', href: '/favicon.png' }]]
+
+const sidebar = {
+  '/information/': [
+    {
+      text: '课程信息',
+      children: [{ text: '课程介绍', link: '/information/introduction' }]
+    }
+  ]
+}
+
+export default defineUserConfig({
+  head,
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN',
+      title: 'Python编程与数据分析',
+      description: '北京师范大学本科生通识教育课程'
+    }
+  },
+  theme: defaultTheme({
+    repo: 'https://github.com/frederick-wang/www.bnupython.com',
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        notFound: ['你似乎来到了没有知识存在的荒原...'],
+        selectLanguageText: '选择语言',
+        selectLanguageName: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+        navbar: [{ text: '课程信息', link: '/information/introduction' }],
+        sidebar
+      }
+    }
+  })
+})
---
+++ docs/README.md
@@ -0,0 +1,22 @@
+---
+home: true
+actions: 
+  - text: 查看课程介绍
+    link: /information/introduction
+    type: primary
+footerHtml: true
+features:
+  - title: 零基础学习
+    details: 无需任何前置知识，通过课程掌握编程基础
+  - title: 数据分析
+    details: 从数据中挖掘宝藏，深入学习数据处理与分析
+  - title: 机器学习
+    details: 助力学习及研究，掌握人工智能领域的机器学习模型
+  - title: 深度学习
+    details: 深入学习深度学习模型，加深人工智能知识
+  - title: 实践编程
+    details: 从数据爬取、预处理及可视化等方面进行编程实践
+  - title: 学术界前沿
+    details: 讲授机器学习、深度学习模型的原理及学术界前沿研究进展
+footer: Copyright © 2022-2023 <a href="https://icip.bnu.edu.cn/" target="_blank">北京师范大学</a>
+---
